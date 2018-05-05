61bfbed73aa022eda8b54a035637c0b450a91194
PavelStatkevych <p.statkevich@gmail.com>
2018-05-05 14:00:04 +0300

    update

--- js/js.js
+++ js/js.js
@@ -6,6 +6,24 @@ $(document).ready(function(){
 		slidesToScroll: 5,
 		arrows: false
 	});
+	$('.feedback-slider').slick({
+		autoplay:true,
+		autoplaySpeed: 3000,
+		dots: true,
+		prevArrow: $('.ar-prev'),
+		nextArrow: $('.ar-next'),
+		dotsClass: 'dots-bottom'
+	});
+	$('.header-slider').slick({
+		autoplay:true,
+		autoplaySpeed: 3000,
+		arrows: false,
+		dots: true,
+		infinite: true,
+		speed: 800,
+		fade: true,
+		cssEase: 'linear'
+	});
 	$('.header-wrapper').addClass('animated bounceInDown');
 	$('.main-head').addClass('animated bounceInLeft');
 	$('.works').addClass('animated bounceInUp');
