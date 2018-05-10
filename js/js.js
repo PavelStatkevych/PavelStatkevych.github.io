d009a17ad5590118aa556ef504ba2a8ba7fe4c3f
PavelStatkevych <p.statkevich@gmail.com>
2018-05-10 14:26:38 +0300

    up

--- js/js.js
+++ js/js.js
@@ -4,7 +4,21 @@ $(document).ready(function(){
 		autoplaySpeed: 3000,
 		slidesToShow: 4,
 		slidesToScroll: 4,
-		arrows: false
+		arrows: false,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			}
+		},
+		{
+			breakpoint: 700,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		}]
 	});
 	$('.feedback-slider').slick({
 		autoplay:true,
