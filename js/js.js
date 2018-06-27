f0cdffebf3ba77838c7c61d19b18ec440fb53338
PavelStatkevych <p.statkevich@gmail.com>
2018-06-27 11:27:10 +0300

    test

--- js/js.js
+++ js/js.js
@@ -1,60 +1,19 @@
 $(document).ready(function(){
-	$('.product-slider-wrap').slick({
-		autoplay:true,
-		autoplaySpeed: 3000,
-		slidesToShow: 4,
-		slidesToScroll: 4,
-		arrows: false,
-		responsive: [{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
-			}
-		},
-		{
-			breakpoint: 700,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
-			}
-		}]
+	$('.slider').slick({
+		dots:true,
+		prevArrow: $('.ar-prew-top'),
+		nextArrow: $('.ar-next-top')
 	});
-	$('.feedback-slider').slick({
-		autoplay:true,
-		autoplaySpeed: 8000,
+	$('.sale-catalog-slider').slick({
 		dots: true,
 		prevArrow: $('.ar-prew'),
 		nextArrow: $('.ar-next'),
-		dotsClass: 'dots-bottom'
+		dotsClass: 'bottom-dots'
 	});
-	$('.header-slider').slick({
-		autoplay:true,
-		autoplaySpeed: 3000,
-		arrows: false,
+	$('.sale-catalog-slider2').slick({
 		dots: true,
-		infinite: true,
-		speed: 800,
-		fade: true,
-		cssEase: 'linear'
-	});
-	var slides = $('.parametr-list');
-	var maxH = 0;
-
-	for (var i = 0; i < slides.length; i++) {
-		if (slides.eq(i).height() > maxH) {
-			maxH = slides.eq(i).height();
-		}
-	}
-	slides.css({
-		'height': maxH
-	});
-	$('.order-btn').click(function(){
-		$('.callback-popup').addClass('show')
-		$('body').addClass('dark')
-	});
-	$('.close-btn').click(function(){
-		$('.callback-popup').removeClass('show')
-		$('body').removeClass('dark')
+		prevArrow: $('.ar-prew2'),
+		nextArrow: $('.ar-next2'),
+		dotsClass: 'bottom-dots'
 	});
 });
