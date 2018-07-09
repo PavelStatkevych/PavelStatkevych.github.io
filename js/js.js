31c83e96f564ea83b1339a704634d4f05b26ccdf
PavelStatkevych <p.statkevich@gmail.com>
2018-07-09 15:14:25 +0300

    up

--- js/js.js
+++ js/js.js
@@ -16,4 +16,29 @@ $(document).ready(function(){
 		nextArrow: $('.ar-next2'),
 		dotsClass: 'bottom-dots'
 	});
+
 });
+var acc = document.getElementsByClassName("fl-cat-name");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
