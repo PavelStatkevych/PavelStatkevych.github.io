3674f1c8adecdf8243bb4e66339a11a408c90b62
PavelStatkevych <p.statkevich@gmail.com>
2018-08-14 17:33:49 +0300

    new

--- js/js.js
+++ js/js.js
@@ -1,71 +1,83 @@
 $(document).ready(function(){
-	$('.slider').slick({
-		dots:true,
-		prevArrow: $('.ar-prew-top'),
-		nextArrow: $('.ar-next-top')
+	$('.product-slider-wrap').slick({
+		autoplay:true,
+		autoplaySpeed: 3000,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		arrows: false,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			}
+		},
+		{
+			breakpoint: 700,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		}]
 	});
-	$('.sale-catalog-slider').slick({
+	$('.feedback-slider').slick({
+		autoplay:true,
+		autoplaySpeed: 8000,
 		dots: true,
 		prevArrow: $('.ar-prew'),
 		nextArrow: $('.ar-next'),
-		dotsClass: 'bottom-dots'
+		dotsClass: 'dots-bottom'
 	});
-	$('.sale-catalog-slider2').slick({
+	$('.header-slider').slick({
+		autoplay:true,
+		autoplaySpeed: 3000,
+		arrows: false,
 		dots: true,
-		prevArrow: $('.ar-prew2'),
-		nextArrow: $('.ar-next2'),
-		dotsClass: 'bottom-dots'
+		infinite: true,
+		speed: 800,
+		fade: true,
+		cssEase: 'linear'
 	});
-	$('.up-btn-wrap').click(function() {      
-        $('body,html').animate({
-            scrollTop : 0                       
-        }, 500);
-    });
-	function wcqib_refresh_quantity_increments() {
-		$("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
-			var c = $(b);
-			c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
-		})
-	}
-	String.prototype.getDecimals || (String.prototype.getDecimals = function() {
-		var a = this,
-		b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-		return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
-	}), $(document).ready(function() {
-		wcqib_refresh_quantity_increments()
-	}), $(document).on("updated_wc_div", function() {
-		wcqib_refresh_quantity_increments()
-	}), $(document).on("click", ".plus, .minus", function() {
-		var a = $(this).closest(".quantity").find(".qty"),
-		b = parseFloat(a.val()),
-		c = parseFloat(a.attr("max")),
-		d = parseFloat(a.attr("min")),
-		e = a.attr("step");
-		b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
-	});
+	var slides = $('.parametr-list');
+	var maxH = 0;
 
-});
-var acc = document.getElementsByClassName("fl-cat-name");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function() {
-		var panel = this.nextElementSibling;
-		if (panel.style.display === "block") {
-			panel.style.display = "none";
-		} else {
-			panel.style.display = "block";
+	for (var i = 0; i < slides.length; i++) {
+		if (slides.eq(i).height() > maxH) {
+			maxH = slides.eq(i).height();
 		}
+	}
+	slides.css({
+		'height': maxH
 	});
+	$('.order-btn').click(function(){
+		$('.callback-popup').addClass('show')
+		$('body').addClass('dark')
+	});
+	$('.close-btn').click(function(){
+		$('.callback-popup').removeClass('show')
+		$('body').removeClass('dark')
+	});
+});
+// Card slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
-for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var panel = this.nextElementSibling;
-		if (panel.style.maxHeight){
-			panel.style.maxHeight = null;
-		} else {
-			panel.style.maxHeight = panel.scrollHeight + "px";
-		} 
-	});
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("card-slide");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
